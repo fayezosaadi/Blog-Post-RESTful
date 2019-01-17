273b84e8de6eb10220aa5fdd5409a1895be8c421
Fayez Saadi <fayez@hackcapital.com>
2019-01-16 16:59:22 -0800

    add Dockerfile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Get Home page
 router.get("/", (req, res) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "EA Games Reviews" });
 });
 
 module.exports = router;
