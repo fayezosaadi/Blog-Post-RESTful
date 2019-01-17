b549c9dd71a21d20e27cdd0bf360ffd4a19d4237
Fayez Saadi <fayez@hackcapital.com>
2019-01-17 13:38:37 -0800

    refactor code

--- routes/posts.js
+++ routes/posts.js
@@ -1,87 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const cuid = require("cuid");
-const filter = require("lodash/filter");
-
-const examplePostId = cuid();
-// initial data
-let postsLists = [
-  {
-    id: examplePostId,
-    author: "Anonymous",
-    title: "EA newest game",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's " +
-      "standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make " +
-      "a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, " +
-      "remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing " +
-      "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions " +
-      "of Lorem Ipsum."
-  }
-];
-
-let commentsList = [
-  {
-    id: 1,
-    postId: examplePostId,
-    author: "Anonymous",
-    content: "This is by far the best blog post for this game."
-  },
-  {
-    id: 2,
-    postId: examplePostId,
-    author: "Anonymous",
-    content: "A 5 stars post"
-  }
-];
-
-// create post method
-const createPost = async ({ author, title, content }) => {
-  const id = cuid();
-  await postsLists.push({
-    id,
-    author,
-    title,
-    content
-  });
-};
-
-// create comment method
-const createComment = async ({ postId, author, content }) => {
-  const id = cuid();
-  await commentsList.push({
-    id,
-    postId,
-    author,
-    content
-  });
-};
-
-const getPost = ({ id }) => ({
-  post: filter(postsLists, { id })[0]
-});
-
-const getComments = ({ postId }) => ({
-  commentsList: filter(commentsList, { postId })
-});
+const {
+  getPosts,
+  getComments,
+  getPost,
+  createPost,
+  createComment
+} = require("../queries/posts");
 
 // API
 // get list of posts
 router.get("/api", (req, res) => {
-  res.status(200).send({ postsLists });
+  res.status(200).send({ postsLists: getPosts() });
 });
 
 // get a single post
-router.get("/api/:id", (req, res) => {
+router.get("/api/:id", async (req, res) => {
   const { id } = req.params;
-  const { post } = getPost({ id });
+  const { post } = await getPost({ id });
   res.status(200).send({ post });
 });
 
 // get list of comments related to a post
-router.get("/api/comments/:id", (req, res) => {
+router.get("/api/comments/:id", async (req, res) => {
   const { id } = req.params;
-  const { commentsList } = getComments({ postId: id });
+  const { commentsList } = await getComments({ postId: id });
   res.status(200).send({ commentsList });
 });
 
@@ -117,7 +60,7 @@ router.post("/api/comments", async (req, res) => {
 
 /* GET posts listing. */
 router.get("/", (req, res) => {
-  res.render("posts", { postsLists });
+  res.render("posts", { postsLists: getPosts() });
 });
 
 /* GET create new post page */
@@ -126,10 +69,10 @@ router.get("/new", (req, res) => {
 });
 
 // get a single post
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const { post } = getPost({ id });
-  const { commentsList } = getComments({ postId: id });
+  const { post } = await getPost({ id });
+  const { commentsList } = await getComments({ postId: id });
   res.render("post", { post, commentsList });
 });
 
